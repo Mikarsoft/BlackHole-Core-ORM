2158d7941e3c4c0a5d604e3cb8643e21a3a6241b
Archolator <archolekas.mich@gmail.com>
2023-06-28 08:39:30 +0100

    updated license

--- BlackHole/Services/BHInitialData.cs
+++ BlackHole/Services/BHInitialData.cs
@@ -3,14 +3,16 @@
 namespace BlackHole.Services
 {
     /// <summary>
-    /// 
+    /// An interface that can be used , to  insert default data and 
+    /// create stored procedures in a newly created database.
     /// </summary>
     public interface IBHInitialData
     {
         /// <summary>
-        /// 
+        /// A contact method that allows to insert default data and 
+        /// create stored procedures in a newly created database
         /// </summary>
-        /// <param name="initializer"></param>
+        /// <param name="initializer">A data provider that accepts custom sql commands or sql files</param>
         void DefaultData(BHDataInitializer initializer);
     }
 }
