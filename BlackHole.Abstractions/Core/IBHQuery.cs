5469ad6179fb2bdb0a537861f167f179d0ae5424
Archolator <archolekas.mich@gmail.com>
2024-06-08 13:41:01 +0300

    grouping approach

--- BlackHole.Abstractions/Core/IBHQuery.cs
+++ BlackHole.Abstractions/Core/IBHQuery.cs
@@ -39,6 +39,37 @@ namespace BlackHole.Abstractions.Core
         /// </summary>
         /// <returns></returns>
         Task<List<T>> ToListAsync();
+
+        public static IEnumerable<IGrouping<TKey, TElement>> GroupBy<TSource, TKey, TElement>(
+    this IEnumerable<TSource> source,
+    Func<TSource, TKey> keySelector,
+    Func<TSource, TElement> elementSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+            if (elementSelector == null) throw new ArgumentNullException(nameof(elementSelector));
+
+            var lookup = new Dictionary<TKey, List<TElement>>();
+
+            foreach (var item in source)
+            {
+                var key = keySelector(item);
+                var element = elementSelector(item);
+
+                if (!lookup.TryGetValue(key, out var elements))
+                {
+                    elements = new List<TElement>();
+                    lookup[key] = elements;
+                }
+
+                elements.Add(element);
+            }
+
+            foreach (var pair in lookup)
+            {
+                yield return new Grouping<TKey, TElement>(pair.Key, pair.Value);
+            }
+        }
     }
 
     /// <summary>
@@ -48,6 +79,53 @@ namespace BlackHole.Abstractions.Core
     /// <typeparam name="G"></typeparam>
     public interface IBHGroup<T, G>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        IBHQuery<T> OrderByAscending(Expression<Func<T, object?>> action);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        IBHQuery<T> OrderByDescending(Expression<Func<T, object?>> action);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        List<T> ToList();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<List<T>> ToListAsync();
+    }
+
+
+    private class Grouping<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        public TKey Key { get; }
+        private readonly IEnumerable<TElement> _elements;
+
+        public Grouping(TKey key, IEnumerable<TElement> elements)
+        {
+            Key = key;
+            _elements = elements;
+        }
+
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            return _elements.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _elements.GetEnumerator();
+        }
     }
 }
